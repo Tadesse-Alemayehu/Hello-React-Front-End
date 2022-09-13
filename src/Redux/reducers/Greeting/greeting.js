3f77f9ece41c7ad7561002970fe80ee9f15b55cb
Tadesse-Alemayehu <email24t@gmail.com>
2022-09-13 22:25:07 +0300

    Configure: action and dispatches

--- src/Redux/reducers/Greeting/greeting.js
+++ src/Redux/reducers/Greeting/greeting.js
@@ -1,7 +1,14 @@
 const RANDOM_GREET_ACTION = "Greeting/greeting/RANDOM_GREET_ACTION";
 
 export const generateGreeting = () => (dispatch) => {
-  fetch("localhost/4000/greeting")
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  myHeaders.append("Accept", "application/json");
+  fetch("http://localhost:4000/greeting", {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  })
     .then((response) => response.json())
     .then((data) =>
       dispatch({ type: RANDOM_GREET_ACTION, payload: data.greeting })
--- src/components/greeting.js
+++ src/components/greeting.js
@@ -1,5 +1,15 @@
 import React from "react";
-
+import { useSelector, useDispatch } from "react-redux";
+import { generateGreeting } from "../Redux/reducers/Greeting/greeting";
 export default function Greeting() {
-  return <div>Soon Ill greet you </div>;
+  const dispatch = useDispatch();
+  const greeting = useSelector((state) => state.Greeting);
+  return (
+    <div>
+      {greeting}
+      <button onClick={() => dispatch(generateGreeting())}>
+        Click here to greet
+      </button>
+    </div>
+  );
 }
